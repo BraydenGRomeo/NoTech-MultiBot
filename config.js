d6b2a2fb959974ddf8fb094fbcdd7aee0c38dfeb
Axxxmir <81544138+Axxxmir@users.noreply.github.com>
2021-08-27 22:37:59 -0500

    Update config.js

--- config.js
+++ config.js
@@ -10,6 +10,22 @@ message.member.roles.add(role)
 }
 if (message.content === 'Insert here') {
  message.reply(message.author.id());
+}
+if(message.content.startsWith("command here")) {
+ const whattosend = message.content.slice("".length).trim().split(/ +/);
+whattosend.shift().toLowerCase().split(" ")[1]
+const member = message.mentions.members.first() || message.guild.members.cache.get(whattosend[0])
+if(!member) return message.channel.send('Provide a user!')
+if(!whattosend[1]) return message.channel.send('what to send')
+member.send(whattosend.slice(1).join(" "))
+}
+if(message.content.startsWith("badword here")) {
+message.delete()
+message.channel.send("bots reply")
+}
+
+
+
 })
 
  client.login("paste your token here")
